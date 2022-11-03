9f23fd2aaef0055ca002904e0765643e078d8a06
DMNGS <samuel.dmngs@eduge.ch>
2022-11-03 16:06:23 +0100

    Added second level with distance sensor

--- app/enigma.py
+++ app/enigma.py
@@ -1,5 +1,6 @@
 import pygame
 import RPi.GPIO as GPIO
+import time
 
 pygame.init()
 
@@ -16,13 +17,18 @@ PIN_LEFT = 25
 PIN_DOWN = 13
 PIN_UP = 26
 PIN_RIGHT = 19
+TRIG = 16
+ECHO = 12
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(PIN_LEFT, GPIO.IN)
 GPIO.setup(PIN_DOWN, GPIO.IN)
 GPIO.setup(PIN_UP, GPIO.IN)
 GPIO.setup(PIN_RIGHT, GPIO.IN)
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
 screen = pygame.display.set_mode([SCREEN_WIDTH, SCREEN_HEIGHT])
+font = pygame.font.SysFont('freesansbold.ttf', 32)
 
 down_color = COLOR_OFF
 left_color = COLOR_OFF
@@ -30,8 +36,11 @@ right_color = COLOR_OFF
 up_color = COLOR_OFF
 pressed = True
 guess = ''
+
+level = 0
  
 running = True
+# Level 1 : Make a code with the D-pad on the bottom right of the CrowPi 
 def d_pad_code():
     global pressed, guess
     solved = False
@@ -40,11 +49,13 @@ def d_pad_code():
     up_color = COLOR_ON if not GPIO.input(PIN_UP) else COLOR_OFF    
     right_color = COLOR_ON if not GPIO.input(PIN_RIGHT) else COLOR_OFF
     
+    # Check which button was pressed
     guess += 'L' if not GPIO.input(PIN_LEFT) and not pressed else ''    
     guess += 'D' if not GPIO.input(PIN_DOWN) and not pressed else ''    
     guess += 'U' if not GPIO.input(PIN_UP) and not pressed else ''    
     guess += 'R' if not GPIO.input(PIN_RIGHT) and not pressed else ''
     
+    # Only check input only when pressed
     pressed = True if not GPIO.input(PIN_LEFT) or not GPIO.input(PIN_DOWN) or not GPIO.input(PIN_UP) or not GPIO.input(PIN_RIGHT) else False
     
     if len(guess) > 0 and guess[len(guess)-1] != CODE[len(guess)-1]:
@@ -53,8 +64,7 @@ def d_pad_code():
     if guess == CODE:
         solved = True
     
-    print(guess)
-    
+    # Draw D-Pad
     screen.fill((255, 255, 255))
     pygame.draw.circle(screen, left_color, (100, 250), 75)
     pygame.draw.circle(screen, down_color, (250, 400), 75)
@@ -62,13 +72,57 @@ def d_pad_code():
     pygame.draw.circle(screen, right_color, (400, 250), 75)
     
     return solved
+
+# Level 2 : Put your hand at the right distance form the sensor
+def distance():
+    RIGHT_DITANCE = 500
+    GOAL_DIST = 25
+    DELTA = 1
+    cur_dist = 0
+    right_dist = False
+    
+    # Wait for sensor to settle
+    GPIO.output(TRIG, False)
+    time.sleep(2)
+
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+        
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
+
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+    
+    text = font.render(str(distance), True, (0, 0, 0 ))
+    text_rect = text.get_rect();
+    text_rect.center = (250, 250)
+    
+    screen.fill((255, 255, 255))
+    screen.blit(text, text_rect)
+    
+    if distance < GOAL_DIST + DELTA and distance > GOAL_DIST - DELTA:
+        right_dist = True
+        
+    return right_dist
     
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     
-    if d_pad_code():
+    if level == 0:
+        if d_pad_code():
+            level = 1
+    elif level == 1:
+        if distance():
+            level = 2
+    else:
         running = False
             
     pygame.display.flip()
